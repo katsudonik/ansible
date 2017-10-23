cb4edc6703f6a77255940a46fa4ed0648cac5fe4
katsudonik <23866351+katsudonik@users.noreply.github.com>
2017-10-23 15:16:11 +0900

    .

--- roles/middleware/elastic_docker/templates/myapp.sh
+++ roles/middleware/elastic_docker/templates/myapp.sh
@@ -1,3 +1,3 @@
 #service tomcat start
-cd /usr/local/vc/elasticsearch_java_sample; git pull
+cd /usr/local/vc/elasticsearch_java_sample; git pull; gradle build
 java -jar /usr/local/vc/elasticsearch_java_sample/build/libs/elasticsearch_java_sample-0.0.1-SNAPSHOT.jar
