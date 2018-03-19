369e0fb59cb2d1427dee5d11df2d43c855e88f34
katsudonik <23866351+katsudonik@users.noreply.github.com>
2018-03-19 19:55:20 +0900

    .

---
+++ roles/middleware/python/tasks/other.sh
@@ -0,0 +1,14 @@
+```
+sudo yum -y install git zlib-devel bzip2 bzip2-devel readline-devel sqlite sqlite-devel openssl-devel
+git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+source ~/.bashrc
+pyenv versions
+pyenv install -l | grep anaconda
+pyenv install anaconda3-5.1.0
+pyenv rehash
+pyenv global anaconda3-4.1.0
+python --version
+```
